92c090a1c4c1d1bbef24438ed71b67883eb35396
Rosendo Guzman <rgnogueda@gmail.com>
2020-06-28 02:03:21 -0500

    docs rename

---
+++ docs/precache-manifest.79c75d3d7fd9027ebac18f10070ac4dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83347e92c675fa93fba5000d67e757cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a00e630bc1edbf875192",
+    "url": "/static/css/main.053003f4.chunk.css"
+  },
+  {
+    "revision": "1cca9b55bbfb221a1a5d",
+    "url": "/static/js/2.e122c200.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e122c200.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00e630bc1edbf875192",
+    "url": "/static/js/main.e9839d71.chunk.js"
+  },
+  {
+    "revision": "f1c04d0ff1c1e1d1be53",
+    "url": "/static/js/runtime-main.449490d0.js"
+  }
+]);
